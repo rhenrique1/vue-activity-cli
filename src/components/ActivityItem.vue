1479c621f31c4aff34ef418ba23208f8fafc4725
Roberto H <roohriq@gmail.com>
2021-01-17 12:44:32 -0300

    lesson 18

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -1,28 +1,33 @@
 <template>
-    <article class="post" >
-    <h4>{{activity.title}}</h4>
+  <article class="post">
+    <h4>{{ activity.title }}</h4>
     <div class="media">
-    <div class="media-left">
+      <div class="media-left">
         <p class="image is-32x32">
-        <img src="../assets/user.png">
+          <img src="../assets/user.png">
         </p>
-    </div>
-    <div class="media-content">
+      </div>
+      <div class="media-content">
         <div class="content">
-        <p>
-            <a href="#">Filip Jerga</a> {{activity.updatedAt}} &nbsp;
-        </p>
+          <p>
+            <a href="#">Filip Jerga</a> {{ activity.updatedAt }} &nbsp;
+          </p>
         </div>
-    </div>
-    <div class="media-right">
+      </div>
+      <div class="media-right">
         <span>Progress Bar Here</span>
+      </div>
     </div>
-    </div>
-    </article>
+  </article>
 </template>
 <script>
     export default {
-        props: ['activity']
+        props: {
+            activity: {
+                type: Object,
+                required: true
+            }
+        }
         
     }
 </script>
